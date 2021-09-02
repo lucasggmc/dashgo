50ddfbbacf853988338ab5e00d4018b6615ba159
lucasggmc <lucasggmc@gmail.com>
2021-09-01 23:14:41 -0300

    adicionado responsividade em todas as páginas já criadas

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,18 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 
 import { Sidebar } from "../../components/Sidebar";
 
-export default function UserList(){
-    return(
+export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    })
+
+    return (
         <Box>
             <Header />
 
@@ -14,41 +20,47 @@ export default function UserList(){
                 <Sidebar />
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                    <Flex mb="8" justify="space-between" align="center">                        
+                    <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
-                        <Button 
-                            as="a" 
-                            size="sm" 
-                            fontSize="sm" 
-                            colorScheme="pink" 
-                            leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
-                        >
-                            Criar novo
-                        </Button>
+                        <Link href="/users/create" passHref>
+                            <Button
+                                as="a"
+                                size="sm"
+                                fontSize="sm"
+                                colorScheme="pink"
+                                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                            >
+                                Criar novo
+                            </Button>
+                        </Link>
                     </Flex>
-                
+
                     <Table colorScheme="whiteAlpha">
                         <Thead>
-                            <Tr px="6" color="gray.300" width="8" >
-                                <Th> <Checkbox colorScheme="pink"/> </Th>
+                            <Tr>
+                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
-                                <Th width="8"></Th>
+                                {isWideVersion && <Th>Data de cadastro</Th>}
+                                {/* <Th width="8"></Th> */}
                             </Tr>
                         </Thead>
 
                         <Tbody>
-                            <Tr px="6">
-                                <Td> <Checkbox colorScheme="pink"/> </Td>
+                            <Tr>
+                                <Td px={["4", "4", "6"]}>
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
                                 <Td>
                                     <Box>
                                         <Text fontWeight="bold">Lucas Carneiro</Text>
                                         <Text fontSize="sm" color="gray.300">luasdasda@asdas.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>04 de abril, 2021</Td>
-                                <Td>
+                                {isWideVersion && <Td>04 de abril, 2021</Td>}
+                                {/* <Td>
                                     <Button 
                                         as="a" 
                                         size="sm" 
@@ -56,9 +68,9 @@ export default function UserList(){
                                         colorScheme="purple" 
                                         leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                                     >
-                                        Editar
+                                        { isWideVersion ? "Editar" : "" }
                                     </Button>
-                                </Td>
+                                </Td> */}
                             </Tr>
                         </Tbody>
                     </Table>
