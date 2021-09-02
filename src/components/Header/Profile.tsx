2e0caeeecdf0965d3edc2d04652b8eb3d0b915aa
lucasggmc <lucasggmc@gmail.com>
2021-09-01 22:11:33 -0300

    adicionado SidebarDrawer responsiva

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Lucas Carneiro</Text>
-                <Text
-                    color="gray.300"
-                    fontSize="small"
-                >
-                    asdas@asdas.com
-                </Text>
-            </Box>
+            { showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Lucas Carneiro</Text>
+                    <Text
+                        color="gray.300"
+                        fontSize="small"
+                    >
+                        asdas@asdas.com
+                    </Text>
+                </Box>
+            )}
 
             <Avatar size="md" name="Lucas Carneiro" src="https://github.com/lucasggmc.png" />
         </Flex>
